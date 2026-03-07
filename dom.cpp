046511cf6263d290e4a5c2583c5f0383ef9c90e8
unknown <aleru@bk.ru>
2026-03-08 01:41:00 +0800

    Я все сделал. Я молодец!

---
+++ dom.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Введите ваше число";
+    cin >> num
+    if(num % 2 == 0) {
+        cout << "Число четное";
+    }
+
+    else {
+        cout << "Число нечетное";
+    }
+}
