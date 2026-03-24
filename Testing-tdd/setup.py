14a08ee575c08962a17e75369160861757887193
unknown <aleru@bk.ru>
2026-03-24 16:30:29 +0800

    changed setup.py

---
+++ Testing-tdd/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ndfl",
+    version = "0.0.0",
+    long_description = "Tax calculator",
+    long_description_content_type = "text/markdown",
+    package_dir = {"": "src"},
+    packages = find_packages(where="src")
+)
