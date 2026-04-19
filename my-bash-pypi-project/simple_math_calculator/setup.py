4133e60f7e788bd29fe05fc40239808564349b19
unknown <aleru@bk.ru>
2026-04-19 22:09:33 +0800

    Edited setup.py

--- my-bash-pypi-project/simple_math_calculator/setup.py
+++ my-bash-pypi-project/simple_math_calculator/setup.py
@@ -1,23 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="simple-math-calculator",
-    version="0.1.0",
+    version="0.1.5",
     author="Albert",
-    description="Fluent calculator for cleaner math operations",
-    long_description=open("README.md").read(),
+    description="Fluent interface calculator",
+    long_description="A simple flow-based calculator project",
     long_description_content_type="text/markdown",
-    url="https://github.com/Albert21210/Development_culture",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.8',
-    install_requires=[],
 )
